8ac0b5a27e72c6d14ad8f6d2c3c32dc97b7e56f5
Kabira Singh <yogesh.baghel86@gmail.com>
2021-05-23 14:17:54 +0530

    Starting with expense tracker

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <h2>Title</h2>
+                <div>Amount</div>
+            </div>
+        </div>
+    );
+};
+
+export default ExpenseItem;
